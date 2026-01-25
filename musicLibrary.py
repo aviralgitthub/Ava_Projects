87a337619693b469f66295b78500e54955e40a77
AVIRAL SHARMA <146520355+aviralgitthub@users.noreply.github.com>
2026-01-25 17:14:28 +0530

    Delete musicLibrary.py

--- musicLibrary.py
+++
@@ -1,7 +0,0 @@
-music = {
-    "sapphire":"https://www.youtube.com/watch?v=JgDNFQ2RaLQ",
-    "thodi si daru":"https://www.youtube.com/watch?v=slM5s55Jz0k",
-    "skyfall":"https://www.youtube.com/watch?v=DeumyOzKqgI",
-    "fitoor":"https://www.youtube.com/watch?v=ccw-bhwtLTw&list=RDMM&index=2"
-
-}
